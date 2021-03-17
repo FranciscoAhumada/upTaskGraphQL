a8d1c20659c728674d937eebfb4d6ca5c2b757e7
Francisco Ahumada cortes <kingsnakef@MacBook-Air-de-Francisco.local>
2021-03-17 19:21:15 -0300

    init commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const {ApolloServer} = require('apollo-server');
+const jwt = require('jsonwebtoken');
+require('dotenv').config('variables.env');
+const typeDefs = require('./db/schema');
+const resolvers = require('./db/resolvers');
+
+const conertarDB = require('./config/db');
+
+conertarDB();
+
+const server = new ApolloServer({typeDefs, resolvers, context:({req})=>{
+    const token = req.headers['authorization'] || '';
+    if(token){
+        try {
+            const usuario = jwt.verify(token.replace('Bearer ',''), process.env.SECRETA);
+            return {usuario};
+        } catch (error) {
+            console.log(error);
+        }
+    }
+}});
+
+server.listen({port: process.env.PORT || 4000}).then(({url}) => {
+    console.log(`server listo en la url ${url}`);
+})
